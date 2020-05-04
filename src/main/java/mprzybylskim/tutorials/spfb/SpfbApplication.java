b60e786d59fbc584dcb61cc36993c09f932b4d2d
mprzybylskim <64372287+mprzybylskim@users.noreply.github.com>
2020-05-04 20:35:49 +0200

    before docker

---
+++ src/main/java/mprzybylskim/tutorials/spfb/SpfbApplication.java
@@ -0,0 +1,13 @@
+package mprzybylskim.tutorials.spfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mprzybylskim/tutorials/spfb/SpfbApplicationTests.java
@@ -0,0 +1,13 @@
+package mprzybylskim.tutorials.spfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
